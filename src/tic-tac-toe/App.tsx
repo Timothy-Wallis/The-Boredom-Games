dc0f4ce442b41a8498c0d29feaff196ccd085905
Timothy Wallis <timothywallis@ucmerced.edu>
2026-05-13 17:13:56 -0700

    Add initial Tic Tac Toe game implementation with HTML, CSS, and React components

---
+++ src/tic-tac-toe/App.tsx
@@ -0,0 +1,14 @@
+import React, { type ReactElement } from 'react'
+import './App.css'
+
+const canvas = <canvas id="canvas" width={window.innerWidth - 200} height={(window.innerHeight / 2)}></canvas>
+export default function App() {
+    return (
+        <>
+        <h1>Play Tic Tac Toe!</h1>
+        <hr/>
+        <p>Click the button below to start playing!</p>
+        {canvas}
+        </>
+    )
+}
